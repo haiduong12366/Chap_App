390684bb48c04044ad7abc95d4bcd864bbfce87d
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2024-02-29 20:50:40 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -11,6 +12,10 @@ import connectToMongoDB from "../db/connectToMongoDB.js";
 
 dotenv.config(); // run file env
 const PORT= process.env.PORT || 5000
+const __dirname = path.resolve() // to get url link
+
+
+
 
 // app.get("/",(req,res)=>{
 //     //root route http://localhost:5000/
@@ -22,6 +27,12 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist"))) // use to serve midleware express.static, pathjoin to connect link from dirname with "/frontend/dist"
+
+app.get("*",(req,res)=>{//run frontend in this port 5000
+    res.sendFile(path.join(__dirname,"/frontend/dist/index.html")) // send index.html to client when have any change 
+})
 
 server.listen(PORT, ()=> {
     connectToMongoDB();
